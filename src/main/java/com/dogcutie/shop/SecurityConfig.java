83ae7c0949454e11c9ce96e88e289820345eec10
JaewonP <protectingjw@naver.com>
2021-02-07 16:07:42 +0900

    Security config 기본 설정

--- src/main/java/com/dogcutie/shop/SecurityConfig.java
+++ src/main/java/com/dogcutie/shop/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.dogcutie.shop;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) {
+		//실제 인증을 진행할 Provider
+	}
+	
+	
+	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/ib/**");
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//Http 관련 보안 설정 
 	}
 }
